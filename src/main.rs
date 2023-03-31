0467c0e45294541ddf22dae4ecbbe26af0e8b925
Afolabi Ajibade <afolabi.ajibade@faculty.ai>
2023-03-31 23:26:34 +0100

    added new constants to evaluation

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,18 @@ extern crate lazy_static;
 
 pub mod poker;
 use crate::poker::evaluation::eval_card;
-use crate::poker::evaluation::lookup;
+use crate::poker::evaluation::lookup::{
+    MAX_TO_RANK_CLASS
+};
 
 
 fn main(){
-    println!("{:?}", lookup::MAX_TO_RANK_CLASS.get(&10).unwrap())
+    let byte1:i32 = 0xF000;
+    let byte2:i32 = 0xF000;
+    let bitwise_and:i32 = byte1 & byte2;
+    println!("{:?}", MAX_TO_RANK_CLASS.get(&10).unwrap());
+    println!("{:b}", byte1);
+    println!("{:b}", byte2);
+    println!("{:b}", bitwise_and);
+
 }
